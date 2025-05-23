198d62a23888aac95566b2a4911ba247b2b3540c
KIMRAHUI <rho0531@naver.com>
2025-05-23 19:51:32 +0900

    Fix: rename CSS to InspectionStyle.css and update import

--- src/pages/Inspection.jsx
+++ src/pages/Inspection.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import axios from 'axios';
-import '../styles/Inspect.css';
+import '../styles/InspectStyle.css';
 
 function Inspection() {
   const [items, setItems] = useState([]);
