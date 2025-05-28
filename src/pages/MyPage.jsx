f17b3e77a3b31d31a32b62f2b611294dccab560e
KIMRAHUI <rho0531@naver.com>
2025-05-28 15:36:48 +0900

    🔥 fix: remove key prop from AutoShopMap to prevent forced re-initialization in MyPage

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -186,7 +186,8 @@ function MyPage() {
           <button onClick={handleReservation}>예약하기</button>
         </div>
 
-       <AutoShopMap key={user?.carNumber} keyword="정비소" onSelectShop={(shop) => setReservationShop(shop)} />
+       <AutoShopMap keyword="정비소" onSelectShop={(shop) => setReservationShop(shop)} />
+
 
 
         <div style={{ marginTop: '1.5rem' }}>
